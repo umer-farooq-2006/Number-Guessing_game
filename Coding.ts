a4d17998e5e2d0bceebdb636472f890b438e43c0
umer-farooq-2006 <umerfarooq13000@gmail.com>
2024-04-21 14:53:36 +0500

    first commit

---
+++ Coding.ts
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const result = await inquirer.prompt([
+    { name : "UserGuessedNumber",
+      type : "number",
+      message : "Please select number between 1 to 5" }
+]);
+if(result.UserGuessedNumber === randomNumber){
+    console.log("Congratulations! You guessed a correct number");
+} else{
+    console.log("Sorry! You guessed a wronge number please try again");
+}
